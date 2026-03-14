773d3b6d3b5cdaafdc6cbb46e520ef005c2aa5c1
zainahmadlums <27100215@lums.edu.pk>
2026-03-14 07:40:08 +0500

    added failing countcities test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -20,4 +20,14 @@ public class CustomListTest {
         list.deleteCity(calgary);
         assertFalse(list.hasCity(calgary));
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
+    }
 }
