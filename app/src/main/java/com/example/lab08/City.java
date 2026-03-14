798d1d45746d07804e0953835a6794430f483e9a
zainahmadlums <27100215@lums.edu.pk>
2026-03-14 06:17:18 +0500

    added comment

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -3,7 +3,7 @@ package com.example.lab08;
 public class City {
     private String city;
     private String province;
-
+    //testcomment
     public City(String city, String province){
         this.city = city;
         this.province = province;
